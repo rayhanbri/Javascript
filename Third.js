89825da99ef1a33b7790f70c8682bc3f67013112
rayhan <mohammadrayhanx0@gmail.com>
2025-09-27 17:03:40 +0600

    third day added.js

---
+++ Third.js
@@ -0,0 +1,194 @@
+// reverse 
+
+// const numbers = [1,2,3,4]
+// console.log(numbers.reverse())
+
+
+// const reverseArray=[];
+
+// for(const num of numbers){
+//     reverseArray.unshift(num)
+// }
+
+// console.log(reverseArray)
+
+//reverse //this is very interestings
+// for(let i = numbers.length-1;i >=0;i--){
+//     const num = numbers[i];
+//     console.log(num)
+// }
+
+
+// const person = ["a","e","c","d"]
+// console.log(person.sort())
+
+//Assending : small to larger
+//Decending : larget to small
+
+// birat jhamela why? 
+//a to z A to Z ascii code. 
+// const numbers = [1,2,3,44,12,20,22];
+
+// console.log(numbers.sort())
+
+// const asending = numbers.sort(function(a,b){return a-b})
+// console.log(asending)
+// const desending = numbers.sort(function(a,b){return b-a})
+// console.log(desending)
+
+// github array-looping-task 
+
+
+/*                  Function syntex                */
+
+//what is function why we use it 
+
+//why there is an undefined fuction 
+
+// function offthefan (){
+//     console.log('fan off kore');
+//     console.log('fan off kore dhur ho ')
+// }
+// // console.log(offthefan)
+// console.log(offthefan())
+
+
+//what is parameter 
+
+// function add(a,b,c){
+//     const adds = a + b + c;
+//     console.log(adds)
+// }
+
+// add(1,2)    //nan 
+// add(1,2,3)
+
+
+
+//what is argument ?
+
+
+//return
+
+// function add(a,b,c){
+//     const adds = a + b + c;
+//     return adds;
+// }
+
+// const addNumber = add(1,2,9)
+// console.log(addNumber)
+
+// function isOdd (a){
+//     if(a % 2 === 0){
+//         return true;
+//     }
+//     return false ;
+// }
+
+// console.log(isOdd(5))
+
+
+// sum of even numbers 
+
+// function evenNumbers(numbers){
+//     let sum = 0;
+//     for(const number of numbers){
+//         if(number % 2 !== 0 ){
+//             sum = sum + number
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(evenNumbers([1,2,3]))
+
+// github js-function-pracitce-tasks
+
+// What is a JavaScript function? 
+
+// A block of code that performs a specific task
+
+
+// In JavaScript, what is the primary difference between a function and a loop?
+// A loop is used for iteration, while a function is for code organization.
+
+
+// What is an argument in the context of a JavaScript function?
+// A value passed to a function when calling it 
+
+
+// why undefined ? 
+// function getMenu() {
+//  console.log("Burger, Pizza, Pasta");
+// }
+
+// console.log(getMenu())
+
+
+// In JavaScript, what happens if a function does not contain a "return" statement?
+
+// It automatically returns "undefined"
+
+// function inchtoFeet(number){
+//     const feetFloat = number / 12;
+//     const feet = parseInt(feetFloat)
+//     // const inches = feetFloat - feet; 
+//     const inches = number % 12;
+
+//     return {feet , inches}
+// }
+
+// console.log(inchtoFeet(75))
+
+
+//leap year ber kora 
+
+// function isLeapYear(year){
+//     // && logic diye kora jaitho bolod shala  
+//     if(year % 100 !== 0){
+//         if(year % 4 === 0){
+//             return 'yes this is a lear year';
+//         }
+//     }
+//     if(year % 100 === 0 && year % 400 === 0){
+//         return true;
+//     }
+//   return false ;
+// }
+
+// console.log(isLeapYear(2400))
+
+
+///give me the average of odd number of an array ;
+
+
+// function averageOfOddNumber(number){
+//     let sum = 0
+//     for(let num of number){
+//         if(num % 2 !== 0){
+//             sum = num + sum;
+//         }
+//     }
+//     const average = sum / number.length;
+//     return average
+
+// }
+
+// console.log(averageOfOddNumber([1,2]))
+
+
+//remove duplicate of an array 
+
+// function unique(number){
+//     // console.log(number)
+//   let uniqueArray = [];
+//  for(const num of number){
+//     // ei code e jamela ache 
+//     if(uniqueArray.includes(num) === false){
+//         uniqueArray.push(num)
+//     }
+//  }
+//   return uniqueArray
+// }
+
+// console.log(unique([1,2,3,3]))
